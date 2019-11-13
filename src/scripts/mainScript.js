0a8c8d7c822f1b0e79e21d7d529dca8f5e4b4755
Teo Winkler <teo.winkler@gmail.com>
2019-11-13 18:57:44 +0100

    successfuly implemented buttons

--- src/scripts/mainScript.js
+++ src/scripts/mainScript.js
@@ -1,17 +1,18 @@
-function open(event, index) {
+function openTab(event, index) {
     let i, tabContent, tabLinks;
 
     tabContent = document.getElementsByClassName("tabContent");
-    for (i=0; i < tabContent.length; i++) {
+    for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none"
     }
+    
 
-    tabLinks = documents.getElementsByClassName("tabLinks");
-    for (i=0; i < tabLinks.length; i++) {
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
         tabLinks[i].className = tabLinks[i].className.replace(" active", "");
     }
     
-    document.getElementById(index).style.display = "contents";
+    document.getElementById(index).style.display = "block";
     event.currentTarget.className += " active";
     
 
