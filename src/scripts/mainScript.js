d6167980ef185dd07b694479c6733748d11abddf
Teo Winkler <teo.winkler@gmail.com>
2019-11-19 20:56:26 +0100

    fixed tetxt/img display problem; autoscroll doesn't work

--- src/scripts/mainScript.js
+++ src/scripts/mainScript.js
@@ -11,8 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 else {
                     content.classList.remove("active"); 
                 }
-                console.log(content)
-
             });
         }
     });
