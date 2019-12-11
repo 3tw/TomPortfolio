4a643a9341b494e41bce51a205757c649fdcad4a
Teo Winkler <teo.winkler@gmail.com>
2019-12-12 00:01:39 +0100

    transformed flex to grid; fixed media-screen; added JS function to move arrow buttons

--- src/scripts/mainScript.js
+++ src/scripts/mainScript.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
             const targetContent = document.querySelector(e.target.dataset.target);
             tabContents.forEach(function(content) {
                 if (content == targetContent) {
-                    let targetId = targetContent.getAttribute("id")
                         content.classList.add("active");
                 } 
                 else {
@@ -17,6 +16,34 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         }
     });
+
+    //"move" arrow buttons
+    const smallScreen = window.matchMedia("(max-width: 800px")
+
+    smallScreen.addListener(function() {
+        const bigArrows = document.querySelectorAll(".bigScreenArrow");
+        const smallArrows = document.querySelectorAll(".smallScreenArrow")
+        if (smallScreen.matches) {
+            bigArrows.forEach(function(button) {
+                button.classList.remove("active")
+            });
+            smallArrows.forEach(function(button2) {
+                button2.classList.add("active")
+                console.log(button2)
+            })
+        } else {
+            bigArrows.forEach(function(button) {
+                button.classList.add("active")
+            });
+            smallArrows.forEach(function(button2) {
+                button2.classList.remove("active")
+                console.log(button2)
+
+            })
+        }
+    });
+
+
 
 
 });
