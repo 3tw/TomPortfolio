84c4ead8b566a4c01781f039ca5b73c0fa4fb3c0
Teo Winkler <teo.winkler@gmail.com>
2020-02-25 11:19:42 +0100

    remove unnecessary code (js dipslay arrowbtn)

--- js/mainScript.js
+++ js/mainScript.js
@@ -26,37 +26,4 @@ document.addEventListener('DOMContentLoaded', function () {
     
     });
 
-    //media queries
-    if (matchMedia){
-        const smallScreen = window.matchMedia("(max-width: 830px)");
-        const bigScreen = window.matchMedia("(min-width: 830px)");
-
-
-        smallScreen.addListener(moveArrows);
-        moveArrows(smallScreen);
-    }
-
-    // move arrows
-    function moveArrows(smallScreen) {
-        const bigArrows = document.querySelectorAll(".bigScreenArrow");
-        const smallArrows = document.querySelectorAll(".smallScreenArrow");
-        
-        bigArrows.forEach(function(button){
-            if (smallScreen.matches) {
-                button.classList.remove("active");
-            } 
-            else {
-                button.classList.add("active");
-            }
-        });
-        smallArrows.forEach(function(button2){
-            if (smallScreen.matches) {
-                button2.classList.add("active");
-            }
-            else {
-                button2.classList.remove("active");
-            }
-        });
-    }
-
 });
